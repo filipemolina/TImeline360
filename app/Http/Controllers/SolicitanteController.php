38442ece33ad786c8b34ab6820b5ee525abd30bd
marcelomirandasilva <marcelo.miranda.pp@gmail.com>
2017-08-07 17:02:09 -0300

    Rota edit solicitante

--- app/Http/Controllers/SolicitanteController.php
+++ app/Http/Controllers/SolicitanteController.php
@@ -9,6 +9,13 @@ use Illuminate\Http\Request;
 
 class SolicitanteController extends Controller
 {
+    public function __construct(Solicitante $solicitante)
+    {
+        $this->solicitante = $solicitante; 
+        
+        // todas as rotas aqui serão antes autenticadas
+        $this->middleware('auth');
+    }
     public function index()
     {
         //
@@ -26,12 +33,19 @@ class SolicitanteController extends Controller
 
     public function show($id)
     {
-        //
+
+
+
     }
 
     public function edit($id)
     {
-        //
+        $solicitante = $this->solicitante->find($id);
+        
+        dd($solicitante);
+
+        //return view('solicitantes.edit',compact('solicitante'));
+
     }
 
     public function update(Request $request, $id)
